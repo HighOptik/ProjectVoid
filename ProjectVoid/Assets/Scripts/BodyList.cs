ca488edef141499f0c441a3667066fcba24ed9f1
Mike <mikeemorra@gmail.com>
2018-06-11 09:08:53 -0400

    MINOR
    -first set of bodies
    -almost 2 sets of heads
    -randomizer button

---
+++ ProjectVoid/Assets/Scripts/BodyList.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyList : MonoBehaviour
+{
+    public List<GameObject> AllSprites;
+    public int currentlyDisplayed;
+    // Use this for initialization
+    void Start()
+    {
+        foreach (Transform child in transform)
+        {
+            AllSprites.Add(child.gameObject);
+
+        }
+        currentlyDisplayed = Random.Range(0, AllSprites.Count);
+        DisplayCurrent();
+    }
+
+    // Update is called once per frame
+    public void DisplayCurrent()
+    {
+        foreach (GameObject go in AllSprites)
+        {
+            go.SetActive(false);
+        }
+        AllSprites[currentlyDisplayed].SetActive(true);
+    }
+    public void Next()
+    {
+        currentlyDisplayed = Random.Range(0, AllSprites.Count);
+        DisplayCurrent();
+    }
+}
