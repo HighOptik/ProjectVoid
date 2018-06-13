37c620bb549eac5c69aa416df7a82da53de69a48
Mike <mikeemorra@gmail.com>
2018-06-13 10:04:49 -0400

    MAJOR
    started body sprites
    finished 20 head sprites
    lighting fixes

--- ProjectVoid/Assets/Scripts/Torch.cs
+++ ProjectVoid/Assets/Scripts/Torch.cs
@@ -1,40 +1,73 @@
-﻿using System.Collections;
+﻿using UnityEngine;
 using System.Collections.Generic;
-using UnityEngine;
 
+// Written by Steve Streeting 2017
+// License: CC0 Public Domain http://creativecommons.org/publicdomain/zero/1.0/
+
+/// <summary>
+/// Component which will flicker a linked light while active by changing its
+/// intensity between the min and max values given. The flickering can be
+/// sharp or smoothed depending on the value of the smoothing parameter.
+///
+/// Just activate / deactivate this component as usual to pause / resume flicker
+/// </summary>
 public class Torch : MonoBehaviour
 {
-    public Light torchLight;
-    public float currentLightRange;
-    public float maxLightRange;
-    public float newLightRange;
-    public float smoothspeed;
-    
-    public bool lightMax;
-    public bool lightMin;
+    [Tooltip("External light to flicker; you can leave this null if you attach script to a light")]
+    public Light light;
+    [Tooltip("Minimum random light intensity")]
+    public float minIntensity = 0f;
+    [Tooltip("Maximum random light intensity")]
+    public float maxIntensity = 1f;
+    [Tooltip("How much to smooth out the randomness; lower values = sparks, higher = lantern")]
+    [Range(1, 50)]
+    public int smoothing = 5;
 
-    // Use this for initialization
+    // Continuous average calculation via FIFO queue
+    // Saves us iterating every time we update, we just change by the delta
+    Queue<float> smoothQueue;
+    float lastSum = 0;
+
+
+    /// <summary>
+    /// Reset the randomness and start again. You usually don't need to call
+    /// this, deactivating/reactivating is usually fine but if you want a strict
+    /// restart you can do.
+    /// </summary>
+    public void Reset()
+    {
+        smoothQueue.Clear();
+        lastSum = 0;
+    }
+
     void Start()
     {
-        torchLight = GetComponentInChildren<Light>();
-        currentLightRange = torchLight.range;
-        smoothspeed = 1f;
-        maxLightRange = 5;
-        newLightRange = Random.Range(0, maxLightRange);
+        smoothQueue = new Queue<float>(smoothing);
+        // External or internal light?
+        if (light == null)
+        {
+            light = GetComponent<Light>();
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (currentLightRange <= newLightRange)
-        {
-            currentLightRange += smoothspeed * Time.deltaTime;
-            torchLight.range += smoothspeed *  Time.deltaTime;
-        }
-        if(currentLightRange == newLightRange)
-        {
-            newLightRange = Random.Range(0, maxLightRange);
+        if (light == null)
+            return;
 
+        // pop off an item if too big
+        while (smoothQueue.Count >= smoothing)
+        {
+            lastSum -= smoothQueue.Dequeue();
         }
+
+        // Generate random new item, calculate new average
+        float newVal = Random.Range(minIntensity, maxIntensity);
+        smoothQueue.Enqueue(newVal);
+        lastSum += newVal;
+
+        // Calculate new smoothed average
+        light.intensity = lastSum / (float)smoothQueue.Count;
     }
+
 }
